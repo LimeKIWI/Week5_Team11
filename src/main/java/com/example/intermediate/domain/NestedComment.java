a035ac31e2fd42a69790303266bf4ccc0cf1afb2
scarlet0o0 <ghd7673@naver.com>
2022-08-26 18:47:55 +0900

    #2 Feat: 대댓글 도메인 클래스 생성

---
+++ src/main/java/com/example/intermediate/domain/NestedComment.java
@@ -0,0 +1,31 @@
+package com.example.intermediate.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class NestedComment extends Timestamped{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @JoinColumn(name = "member_id", nullable = false)
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Member member;
+
+    @JoinColumn(name = "comment_id", nullable = false)
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Comment comment;
+
+    @Column(nullable = false)
+    private String content;
+}
