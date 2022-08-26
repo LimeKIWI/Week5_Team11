c9c9b52031ef8d3affda9924ae05c61e82240bf0
scarlet0o0 <ghd7673@naver.com>
2022-08-27 06:49:18 +0900

    #2 Fix: 객체 이름 실수 수정

--- src/main/java/com/example/intermediate/repository/NestedCommentRepository.java
+++ src/main/java/com/example/intermediate/repository/NestedCommentRepository.java
@@ -7,5 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface NestedCommentRepository extends JpaRepository<NestedComment, Long> {
-    List<Comment> findAllByComment(Comment comment);
+    List<NestedComment> findAllByComment(Comment comment);
+
 }
