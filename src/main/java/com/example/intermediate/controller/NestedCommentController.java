a42775260f022853e863035b8bc9c2dd2872e72a
scarlet0o0 <ghd7673@naver.com>
2022-08-27 06:48:46 +0900

    #2 Add: 컨트롤러에 가져오기 수정 삭제 매소드 추가

--- src/main/java/com/example/intermediate/controller/NestedCommentController.java
+++ src/main/java/com/example/intermediate/controller/NestedCommentController.java
@@ -5,10 +5,7 @@ import com.example.intermediate.controller.response.ResponseDto;
 import com.example.intermediate.service.NestedCommentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -22,5 +19,25 @@ public class NestedCommentController {
     public ResponseDto<?> createNestedComment(@RequestBody CommentRequestDto requestDto, HttpServletRequest request) {
         return nestedCommentService.createNestedComment(requestDto, request);
     }
+
+    @RequestMapping(value = "/api/nestedComment/{id}", method = RequestMethod.GET)
+    public ResponseDto<?> getAllNestedComments(@PathVariable Long id) {
+        return nestedCommentService.getAllNestedCommentByComment(id);
+    }
+
+    @RequestMapping(value = "/api/auth/nestedComment/{id}", method = RequestMethod.PUT)
+    public ResponseDto<?> updateNestedComment(@PathVariable Long id,@RequestBody CommentRequestDto requestDto, HttpServletRequest request) {
+        return nestedCommentService.updateNestedComment(id, requestDto, request);
+    }
+
+    @RequestMapping(value = "/api/auth/nestedComment/{id}", method = RequestMethod.DELETE)
+    public ResponseDto<?> deleteNestedComment(@PathVariable Long id, HttpServletRequest request) {
+        return nestedCommentService.deleteNestedComment(id, request);
+    }
+
+
+
+
+
 
 }
