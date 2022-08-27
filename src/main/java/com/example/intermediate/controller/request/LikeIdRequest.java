59669e36316f7fd4d1ab1e9bb757bae82cb351a2
DongDongShin <tlsehdgk87@naver.com>
2022-08-27 15:59:11 +0900

    필드 이름 변경 likeId:1

--- src/main/java/com/example/intermediate/controller/request/LikeIdRequest.java
+++ src/main/java/com/example/intermediate/controller/request/LikeIdRequest.java
@@ -8,7 +8,7 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
-public class PostIdRequest {
+public class LikeIdRequest {
 
-    private Long postId;
+    private Long likeId;
 }
