fbd9db192918e737435c7e5d398d3e8077a5c23a
DongDongShin <tlsehdgk87@naver.com>
2022-08-27 16:00:01 +0900

    ennu 필드 이름 변경 !!:1

--- src/main/java/com/example/intermediate/domain/Like.java
+++ src/main/java/com/example/intermediate/domain/Like.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Parent;
 
 import javax.persistence.*;
 import java.sql.Time;
@@ -19,10 +20,12 @@ public class Like extends Timestamped {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column
-    private Role_Enum role_enum;
+    private Role_Enum role;
     @Column
-    private Long Parent_Id;//참조 ID
-    @Column
-    private Long Member_Id;
+    private Long pid;//참조 ID
+
+    @JoinColumn(name = "member_id", nullable = false)
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Member member;
 
 }
