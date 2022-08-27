4e9d746856d40af9ec180ba56392ea682ad9dbaf
limeKIWI <pop78978@gmail.com>
2022-08-27 21:35:43 +0900

    #3 대댓글목록을 유저정보로 검색가능하게 추가

--- src/main/java/com/example/intermediate/repository/NestedCommentRepository.java
+++ src/main/java/com/example/intermediate/repository/NestedCommentRepository.java
@@ -1,6 +1,7 @@
 package com.example.intermediate.repository;
 
 import com.example.intermediate.domain.Comment;
+import com.example.intermediate.domain.Member;
 import com.example.intermediate.domain.NestedComment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -8,5 +9,6 @@ import java.util.List;
 
 public interface NestedCommentRepository extends JpaRepository<NestedComment, Long> {
     List<NestedComment> findAllByComment(Comment comment);
+    List<NestedComment> findByMember(Member member);
 
 }
